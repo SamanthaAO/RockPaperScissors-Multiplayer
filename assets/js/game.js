c1d8cd7f67d98d048caa00112f6b512ae9852d05
SamanthaAO <samanthaaorcutt@gmail.com>
2019-05-08 18:58:34 -0700

    created button functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,6 +17,8 @@ var firebaseConfig = {
 //set up variables
 var player1Selected = false;
 var player2Selected = false;
+var player1ChoiceSelected = false;
+var player2ChoiceSelected = false;
 var round = 0;
 var p1 = {
     name: "",
@@ -37,6 +39,7 @@ var p2 = {
 
 
 
+
 $("#createPlayer").on("click", function(event){
     event.preventDefault();
 
@@ -49,11 +52,13 @@ database.ref().set({
          p2: p2,
          player1Selected : true,
          player2Selected : false,
+         player1ChoiceSelected : false,
+         player2ChoiceSelected : false
 
       });
       
-      $("#p1-display").text("Welcome " + p1.name + "! You are Player 1. Click any of the below buttons to make your first choice")
-      $("#p1-display").append("<br><button id='p1Rock'>Rock</button> <button id='p1Paper'>Paper</button> <button id='p1Scissors'>Scissors</button>");
+      $("#display").text("Welcome " + p1.name + "! You are Player 1. Click any of the below buttons to make your first choice")
+      $("#display").append("<br><button id='rock' value='p1' class='gameButton'>Rock</button> <button id='paper' value='p1' class='gameButton'>Paper</button> <button id='scissors' value='p1' class='gameButton'>Scissors</button>");
 
     }
     else if(!player2Selected){
@@ -65,10 +70,12 @@ database.ref().set({
             p2: p2,
             player1Selected : true,
             player2Selected : true,
+            player1ChoiceSelected : false,
+            player2ChoiceSelected : false
         });
         
-        $("#p2-display").text("Welcome " + p2.name + "! You are Player 2. Click any of the below buttons to make your first choice")
-        $("#p2-display").append("<br><button id='p2Rock'>Rock</button> <button id='p2Paper'>Paper</button> <button id='p2Scissors'>Scissors</button>");
+        $("#display").text("Welcome " + p2.name + "! You are Player 2. Click any of the below buttons to make your first choice")
+        $("#display").append("<br><button id='rock' value='p2' class='gameButton'>Rock</button> <button id='paper' value='p2' class='gameButton'>Paper</button> <button id='scissors' value='p2' class='gameButton'>Scissors</button>");
     }
     else{
         $("#display").text("Sorry, the game is full.")
@@ -77,17 +84,62 @@ database.ref().set({
 
 });
 
+$("#display").on("click", ".gameButton", function(){
+    if(this.value === p1 && !player1ChoiceSelected){
+        p1.choice = this.id;
+        console.log(this.id);
+        database.ref().set({
+            round: round,
+             p1: p1,
+             p2: p2,
+             player1Selected : true,
+             player2Selected : true,
+             player1ChoiceSelected : true,
+             player2ChoiceSelected : false
+          });
+    
+          $("#display").text("You chose " + p1.choice);
+    }
+    else if(this.value === p2 && !player2ChoiceSelected){
+        p2.choice = this.id;
+        console.log(this.id);
+        database.ref().set({
+            round: round,
+             p1: p1,
+             p2: p2,
+             player1Selected : true,
+             player2Selected : true,
+             player1ChoiceSelected : true,
+             player2ChoiceSelected : true
+          });
+          $("#display").text("You chose " + p2.choice);
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
 
 database.ref().on("value", function(snapshot) {
 p1.name = snapshot.val().p1.name;
 p2.name = snapshot.val().p2.name;
 player1Selected = snapshot.val().player1Selected;
 player2Selected = snapshot.val().player2Selected;
+
 console.log(p1.name + " player1 ");
 console.log(p2.name + " player2 ");
 }, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
+
+
 
 
 
